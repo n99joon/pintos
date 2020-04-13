7943a49a951baedefd5d2bb3809076a0ac93be77
n99joon <52797682+n99joon@users.noreply.github.com>
2020-04-13 21:50:00 +0900

    Update synch.h

--- synch.h
+++ synch.h
@@ -9,6 +9,7 @@ struct semaphore
   {
     unsigned value;             /* Current value. */
     struct list waiters;        /* List of waiting threads. */
+    int priority;   //added to be used in cond functions
   };
 
 void sema_init (struct semaphore *, unsigned value);
