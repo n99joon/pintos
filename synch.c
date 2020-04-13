ddf4c416fd68f7a87300e9ee1b4cb4fe575924c7
n99joon <52797682+n99joon@users.noreply.github.com>
2020-04-13 17:32:07 +0900

    Update synch.c

--- synch.c
+++ synch.c
@@ -122,12 +122,12 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
   
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters)){
-  	list_sort(&sema->waiters, cmp_priority,0);
-    thread_unblock (list_entry (list_pop_front (&sema->waiters),
-                                struct thread, elem));}
-	
   sema->value++;
+  if (!list_empty (&sema->waiters)){
+  	list_sort(&(sema->waiters), cmp_priority,0);
+    thread_unblock (list_entry (list_pop_front (&sema->waiters), struct thread, elem));}
+	
+  
   intr_set_level (old_level);
   
 }
